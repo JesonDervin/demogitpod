c9d9a8014414de548f2c7d911b3aa3c411142e05
JesonDervin <jeson.dervin@outlook.com>
2022-06-23 15:32:16 +0000

    fail cors

--- WebApi/app.js
+++ WebApi/app.js
@@ -1,10 +1,17 @@
 var express = require("express");
 var app = express();
 
-app.listen(3000,()=>{
-    console.log("server listening on port 3000")
-})
-
-app.get("/time", (req, res, next) => {
-    res.json({"servertime": Date.now()});
-   });
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.get("/", (req, res, next) => {
+  res.json({ servertime: Date.now() });
+});
