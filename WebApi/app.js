51fa0573303499759908992b1314d9532efe08db
JesonDervin <jeson.dervin@outlook.com>
2022-06-24 06:00:27 +0000

    fix api and webapp

--- WebApi/app.js
+++ WebApi/app.js
@@ -1,11 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.listen(3000, () => {
-  console.log("server listening on port 3000");
-});
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -13,5 +9,10 @@ app.use(function (req, res, next) {
   next();
 });
 app.get("/", (req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
   res.json({ servertime: Date.now() });
+});
+
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
 });
